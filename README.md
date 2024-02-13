ea45d7e7eb456cca38d955bfb0855ebb333dce2b
Dynamic-CT <dynamic@suresofttech.com>
2024-02-13 17:12:48 +0900

    기타: 오픈소스 공개 대상 업로드

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CT
 CT 오픈소스 공개용 소스 코드 저장소
+
+### 변경된 파일
+- net.jeeeyul.eclipse.themes.ui/src/net/jeeeyul/eclipse/themes/ui/ApplyPresetAction.java
+- net.jeeeyul.eclipse.themes.ui/src/net/jeeeyul/eclipse/themes/ui/preference/preset/IJTPreset.java
+- net.jeeeyul.eclipse.themes.ui/src/net/jeeeyul/eclipse/themes/ui/preference/preset/internal/ContributedPreset.java
+- net.jeeeyul.eclipse.themes.ui/src/net/jeeeyul/eclipse/themes/ui/preference/preset/internal/JTPresetPreferencePage.java
+- net.jeeeyul.eclipse.themes.ui/src/net/jeeeyul/eclipse/themes/ui/preference/preset/internal/UserPreset.java
+- net.jeeeyul.eclipse.themes.ui/plugin.xml
+- net.jeeeyul.eclipse.themes.ui/src/net/jeeeyul/eclipse/themes/ui/hotswap/CustomThemeGenerator.xtend
+
+### 출처
+https://github.com/jeeeyul/eclipse-themes
---
+++ net.jeeeyul.eclipse.themes.ui/src/net/jeeeyul/eclipse/themes/ui/preference/preset/IJTPreset.java
@@ -0,0 +1,45 @@
+package net.jeeeyul.eclipse.themes.ui.preference.preset;
+
+import java.util.Map;
+import java.util.Properties;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+
+/**
+ * @since 2.1
+ * 
+ * @author Jeeeyul
+ */
+public interface IJTPreset {
+	/**
+	 * 
+	 * @return {@link Properties} that contains settings of this
+	 *         {@link IJTPreset}.
+	 */
+	public Properties getProperties();
+
+	/**
+	 * 
+	 * @return Human readable name for this preset.
+	 */
+	public String getName();
+
+	/**
+	 * 
+	 * @return unique id for this {@link IJTPreset}.
+	 */
+	public String getId();
+
+	/**
+	 * 
+	 * @return A preview image descriptor for this {@link IJTPreset}. It's size
+	 *         should have to be 16 x 16.
+	 */
+	public ImageDescriptor getImageDescriptor();
+	
+	/**
+	 * 
+	 * @return 테마와 관련된 아이콘 경로
+	 */
+	public Map<String, String> getIconPaths();
+}
